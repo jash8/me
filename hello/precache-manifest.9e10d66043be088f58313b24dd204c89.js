82650305fc9a5b866e5aeab43ba5b6e6fbe6c611
josh <kerusher@163.com>
2018-06-18 00:41:36 +0800

    a lots changed

---
+++ hello/precache-manifest.9e10d66043be088f58313b24dd204c89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddc0d813b6b9f830b4de3c17dfd188b6",
+    "url": "index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "78913d5af663e1644e1b",
+    "url": "chunk-vendors.78913d5a.js"
+  },
+  {
+    "revision": "7dc59c584e31de1bf1ca",
+    "url": "app.b646c0cf.css"
+  },
+  {
+    "revision": "7dc59c584e31de1bf1ca",
+    "url": "app.7dc59c58.js"
+  }
+];
