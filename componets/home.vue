2cf6fbed03550d3efa6b2dbeb9b86cde75ea85ec
jkerush <zzh3258@qq.com>
2018-06-20 16:29:17 +0800

    m

---
+++ componets/home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <ul>
+        <li v-for="(item,indx) in home" :key="indx">{{ item }}</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      home:['home1','home2','home3','home4','home5']
+    }
+  }
+};
+</script>
+
+<style scoped>
+  ul,li{
+    list-style-type: lower-roman;
+  }
+</style>
